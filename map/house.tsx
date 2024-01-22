0c05d69cb17582197af630cfd93e17307dd5d508
berenice-lm <114936513+berenice-lm@users.noreply.github.com>
2024-01-22 11:53:06 +0100

    add map

---
+++ map/house.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="house" tilewidth="16" tileheight="16" tilecount="30" columns="5">
+ <image source="house.png" trans="000000" width="81" height="98"/>
+</tileset>
