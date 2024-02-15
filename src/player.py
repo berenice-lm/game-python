2faf3aeef2f6d716766067cacbdea8ddc3049da2
berenice-lm <berenice.le-mao@ign.fr>
2024-02-15 17:19:15 +0100

    ajout de la classe ennemi qui ne fonctionne pas encore

--- src/player.py
+++ src/player.py
@@ -93,6 +93,52 @@ class NPC(Entity):
             rect = pygame.Rect(point.x, point.y, point.width, point.height)
             self.points.append(rect)
 
+class Enemy(Entity):
+
+    def __init__(self, name, nb_points):
+        super().__init__(name, 0, 0)
+        self.nb_points = nb_points
+        self.points = []
+        self.name = name
+        self.speed = 1
+        self.current_point = 0
+
+    def move(self):
+        current_point = self.current_point
+        target_point = self.current_point + 1
+
+        if target_point >= self.nb_points:
+            target_point = 0
+
+        current_rect = self.points[current_point]
+        target_rect = self.points[target_point]
+
+        if current_rect.y < target_rect.y and abs(current_rect.x - target_rect.x) < 3:
+            self.move_down()
+        elif current_rect.y > target_rect.y and abs(current_rect.x - target_rect.x) < 3:
+            self.move_up()
+        elif current_rect.x > target_rect.x and abs(current_rect.y - target_rect.y) < 3:
+            self.move_left()
+        elif current_rect.x < target_rect.x and abs(current_rect.y - target_rect.y) < 3:
+            self.move_right()
+        else:
+            self.move_idle()
+
+        if self.rect.colliderect(target_rect):
+            self.current_point = target_point
+
+    def teleport_spawn(self):
+        location = self.points[self.current_point]
+        self.position[0] = location.x
+        self.position[1] = location.y
+        self.save_location()
+    
+    def load_points(self, tmx_data):
+        for num in range(1, self.nb_points+1):
+            point = tmx_data.get_object_by_name(f"{self.name}_path{num}")
+            rect = pygame.Rect(point.x, point.y, point.width, point.height)
+            self.points.append(rect)
+
 class MovingSprite(AnimateSprite):
     def __init__(self, name, x, y):
         super().__init__(name)
