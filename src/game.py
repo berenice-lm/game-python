8484b4f8a589ffa2a972fa6be396f10ea79419cc
berenice-lm <berenice.le-mao@ign.fr>
2024-10-26 16:52:55 +0200

    map launched but badly

--- src/game.py
+++ src/game.py
@@ -2,6 +2,7 @@ import pygame
 from dialog import DialogBox
 from player import Enemy, MovingSprite, Player, NPC
 from map import MapManager
+import subprocess
 
 class Game:
 
@@ -25,6 +26,10 @@ class Game:
         self.chat_coeur_ini = pygame.image.load('map/cat_heart.png').convert_alpha()
         self.chat_coeur = pygame.transform.scale(self.chat_coeur_ini, (250, 80))
         self.chat_coeur_rect = self.chat_coeur.get_rect(topleft=(30, 20))
+
+        self.maps_icon_ini = pygame.image.load('map/maps_icon.png').convert_alpha()
+        self.maps_icon = pygame.transform.scale(self.maps_icon_ini, (120, 120))
+        self.maps_icon_rect = self.maps_icon.get_rect(topright=(1460, 20))
 
         # self.dialog_test_ini = pygame.image.load('dialogs/dialog_box.png').convert_alpha()
         # self.dialog_test = pygame.transform.scale(self.dialog_test_ini, (40, 30))
@@ -69,19 +74,28 @@ class Game:
 
             # afficher les elements sur l'ecran
             self.screen.blit(self.chat_coeur, self.chat_coeur_rect.topleft)
-
-            # if self.dialog_box.is_reading() or self.map_manager.is_npc_colliding():
-            #     for sprite in self.map_manager.get_group().sprites():
-            #         if isinstance(sprite, NPC):
-            #             sprite.load_bubble(True)
+            self.screen.blit(self.maps_icon, self.maps_icon_rect.topright)
 
             if self.dialog_box.is_reading() or self.map_manager.is_npc_colliding():
                 for sprite in self.map_manager.get_group().sprites():
                     if isinstance(sprite, NPC):
                         sprite.load_bubble(True)
 
-
             pygame.display.flip() #actualiser en temps reel
+
+            # for event in pygame.event.get():
+            #     if event.type == pygame.QUIT:
+            #         self.running = False
+            #     elif event.type == pygame.KEYDOWN:
+            #         if event.key == pygame.K_f:
+            #             self.map_manager.check_npc_collisions(self.dialog_box)
+            #     elif event.type == pygame.MOUSEWHEEL:
+            #         if event.y > 0:  # Zoom in
+            #             self.map_manager.zoom_level += 1
+            #             self.map_manager.get_map().group.zoom += 0.1
+            #         elif event.y < 0:  # Zoom out
+            #             self.map_manager.zoom_level -= 1
+            #             self.map_manager.get_map().group.zoom -= 0.1
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -89,6 +103,9 @@ class Game:
                 elif event.type == pygame.KEYDOWN:
                     if event.key == pygame.K_f:
                         self.map_manager.check_npc_collisions(self.dialog_box)
+                    elif event.key == pygame.K_COMMA:  # Vérifier si la touche "," est pressée
+                        print(", pressed")
+                        subprocess.Popen(['python', 'src\carte.py'])  # Lancer carte.py
                 elif event.type == pygame.MOUSEWHEEL:
                     if event.y > 0:  # Zoom in
                         self.map_manager.zoom_level += 1
